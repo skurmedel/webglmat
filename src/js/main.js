50e77977cb34b5aadf78f8441fcd392dde1d3c08
Simon Otter <simon@otter.se>
2013-08-20 23:47:02 +0200

    Fixed some kind of support for resource loading.

--- src/js/main.js
+++ src/js/main.js
@@ -30,13 +30,56 @@ window.requestAnimFrame = (function(){
           };
 })();
 
-function DefaultDemo()
+function LoadResource(url, cb)
 {
+	$.get(url, cb);
+}
 
+function DefaultDemo(fs_url, vs_url)
+{
+	this.fs_url = fs_url == undefined? null : fs_url;
+	this.vs_url = vs_url == undefined? null : vs_url;
 }
 
 DefaultDemo.prototype =
 {
+	load: function DefaultDemo_load(cb)
+	{
+		var me = this;
+		var fs_cb = function(data) 
+		{
+			me.fs = data;
+			cb();
+		};
+		var vs_cb = function(data) 
+		{
+			me.vs = data; 
+			if (me.fs_url == null)
+			{
+				fs_cb(
+					  "varying vec3 N;"
+					+ "varying vec3 p;"
+					+ "uniform vec3 light_pos;"
+					+ "void main() {"
+					+ "  vec3 L = normalize(p - (viewMatrix * vec4(light_pos, 1.0)).xyz);"
+					+ "  gl_FragColor = vec4(dot(N, L) * vec3(1.0, 0.0, 0.0), 1.0);"
+					+ "}");
+			}
+			else
+				LoadResource(this.fs_url, fs_cb);
+		};
+
+		if (this.vs_url == null)
+		{
+			vs_cb(
+				  "varying vec3 N;"
+				+ "varying vec3 p;"
+				+ "void main() { gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0); p = gl_Position.xyz; N = normalize((normalMatrix * normal)).xyz;}");
+		}
+		else
+			LoadResource(this.vs_url, vs_cb);
+	},
+
 	setup: function DefaultDemo_setup(renderer, scene)
 	{
 		var VIEWPORT = 
@@ -83,23 +126,10 @@ DefaultDemo.prototype =
 
 	createShaders: function DefaultDemo_createShaders()
 	{
-		var vs = 
-			  "varying vec3 N;"
-			+ "varying vec3 p;"
-			+ "void main() { gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0); p = gl_Position.xyz; N = normalize((normalMatrix * normal)).xyz;}";
-		var fs = 
-			  "varying vec3 N;"
-			+ "varying vec3 p;"
-			+ "uniform vec3 light_pos;"
-			+ "void main() {"
-			+ "  vec3 L = normalize(p - (viewMatrix * vec4(light_pos, 1.0)).xyz);"
-			+ "  gl_FragColor = vec4(dot(N, L) * vec3(1.0, 0.0, 0.0), 1.0);"
-			+ "}";
-
 		var material = new THREE.ShaderMaterial(
 			{
-				fragmentShader: fs, 
-				vertexShader: vs, 
+				fragmentShader: this.fs, 
+				vertexShader: this.vs, 
 				uniforms: {
 					"light_pos": { type: 'v3', value: new THREE.Vector3(0.0, 1.0, 0.0) },
 				}
@@ -118,6 +148,7 @@ DefaultDemo.prototype =
 		canvas		an element to add the WebGL context under.
 		bg			an integer specifying a colour, i.e 0xFF00FF
 		demo 		an object with the following methods:
+						load(callback) -> null
 						setup(renderer, scene) -> null
 						onRender(renderer, scene, ms) -> null
 					onRender should call:
@@ -138,17 +169,20 @@ function setupShaderDemoArea(canvas, bg, demo) {
 		demo = new DefaultDemo();
 	}
 
-	demo.setup(renderer, scene);
-
-	var ms = Date.now();
-	function render()
+	demo.load(function ()
 	{
-		requestAnimationFrame(render);
-		
-		var tmp = Date.now();
-		demo.onRender(renderer, scene, tmp - ms);
-		ms = tmp;
-	}
-	render();
+		demo.setup(renderer, scene);
+
+		var ms = Date.now();
+		function render()
+		{
+			requestAnimationFrame(render);
+			
+			var tmp = Date.now();
+			demo.onRender(renderer, scene, tmp - ms);
+			ms = tmp;
+		}
+		render();
+	});
 }
 
