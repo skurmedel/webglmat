2d6ed28472eec1eed26907f65cc524f40e45782c
Simon Otter <simon@otter.se>
2013-09-01 03:04:28 +0200

    Cleaned up metal_fs; removed dead code etc.

--- src/shaders/metal_fs.glsl
+++ src/shaders/metal_fs.glsl
@@ -12,6 +12,18 @@ uniform vec3 light_pos;
 	uniform vec3 cameraPosition;
 */
 
+/*
+	Contains common vectors for different stuff,
+	unclutters the code quite a bit.
+*/
+struct directions
+{
+	vec3 H;
+	vec3 L;
+	vec3 N;
+	vec3 V;
+};
+
 float schlick(float F0, float HdotV)
 {
 	return F0 + (1.0 - F0) * pow((1.0 - HdotV), 5.0);
@@ -23,24 +35,16 @@ float schlick_ior(float ior1, float ior2, float HdotV)
 	return schlick(F0 * F0, HdotV);
 }
 
-float TrowbridgeReitz(float a, float NdotH)
-{
-		float a2 = a * a;
-		float pi = 3.1417;
-		float denominator2 = ((NdotH * NdotH) * (a2 - 1.0) + 1.0);
-		float denominator  = pi * (denominator2 * denominator2);
-		
-		return a2 / denominator;
-}
-
-float DGGXTrowbridgeReitz(float roughness, float NdotH)
+float d_gtr2(float roughness, float NdotH)
 {
 	float a2 = roughness * roughness;
-	float deno = 3.1417 * pow((NdotH * NdotH) * (a2 - 1.0) + 1.0, 2.0);
+	float term1 = 3.1417;
+	float term2 = (NdotH * NdotH) * (a2 - 1.0) + 1.0;
+	float deno = term1 * (term2 * term2);
 	return a2 / deno;
 }
 
-float compute_spec(float ior, float roughness, vec3 H, vec3 L, vec3 N, vec3 V, float F)
+vec3 compute_specular(float ior, float roughness, directions dir, float F)
 {
 	/*
 		Cook-Torrance Microfacet model.
@@ -49,12 +53,13 @@ float compute_spec(float ior, float roughness, vec3 H, vec3 L, vec3 N, vec3 V, f
 		G = geometric attenuation.
 		F = fresnel coefficient.
 	*/
-	float NdotH = max(0.0, dot(N, H));
-	float NdotV = max(0.0, dot(N, V));
-	float VdotH = max(0.0, dot(V, H));
-	float NdotL = max(0.0, dot(N, L));
+	float NdotH = max(0.0, dot(dir.N, dir.H));
+	float NdotV = max(0.0, dot(dir.N, dir.V));
+	float VdotH = max(0.0, dot(dir.V, dir.H));
+	float NdotL = max(0.0, dot(dir.N, dir.L));
 
-	float gauss = 9.0;
+	vec3 R = normalize(p + reflect(dir.L, dir.N));
+	vec3 refl = F * textureCube(env, R).xyz * 2.0;
 
 	float G = min(
 		1.0, 
@@ -62,10 +67,9 @@ float compute_spec(float ior, float roughness, vec3 H, vec3 L, vec3 N, vec3 V, f
 			(2.0 * NdotH * NdotV) / VdotH, 
 			(2.0 * NdotH * NdotL) / VdotH));
 	float a = acos(NdotH);
-	float D = gauss * exp(-(a * a) / (roughness * roughness));
-	// D = DGGXTrowbridgeReitz(roughness, NdotH);
+	float D = d_gtr2(roughness, NdotH);
 
-	return (F * D * G) / (4.0 * NdotL * NdotV);
+	return ((F * D * G) / (4.0 * NdotL * NdotV)) + refl;
 }
 
 /*
@@ -77,29 +81,27 @@ float compute_spec(float ior, float roughness, vec3 H, vec3 L, vec3 N, vec3 V, f
 	L 	incident light vector.
 	F 	fresnel coefficient.
 */
-vec3 compute_diffuse(vec3 Nn, vec3 L, float F)
+vec3 compute_diffuse(directions dir, float F)
 {
-	return (1.0 - F) * vec3(dot(Nn, L));
+	return (1.0 - F) * vec3(dot(dir.N, dir.L));
 }
 
 void main() 
 {
-	vec3 V = normalize(viewMatrix * vec4(cameraPosition - p, 1.0)).xyz;
+	directions dir;
+
+	dir.V = normalize(viewMatrix * vec4(cameraPosition - p, 1.0)).xyz;
 	vec3 Lp = (viewMatrix * vec4(light_pos, 1.0)).xyz;
-	vec3 L = normalize(p - Lp);
-	vec3 H = normalize(L + V);
+	dir.L = normalize(p - Lp);
+	dir.H = normalize(dir.L + dir.V);
 	// The normalized-normal, interpolated normals
 	// might not be unit vectors.
-	vec3 Nn = normalize(N);
-
-	vec3 R = normalize(p + reflect(L, Nn));
+	dir.N = normalize(N);
 	
-	float F = schlick_ior(1.0, ior, dot((L + -V), -V));
+	float F = schlick_ior(1.0, ior, dot((dir.L + -dir.V), -dir.V));
 
-	vec3 refl = F * textureCube(env, R).xyz * 2.0;
+	vec3 diffuse = compute_diffuse(dir, F) * vec3(1.0, 1.0, 1.0);
+	vec3 spec    = compute_specular(ior, roughness, dir, F) * vec3(1.0);
 
-	vec3 diffuse = compute_diffuse(Nn, L, F) * vec3(1.0, 1.0, 1.0);
-	vec3 spec    = compute_spec(ior, roughness, H, L, Nn, V, F) * vec3(1.0);
-
-	gl_FragColor = vec4(diffuse + spec + refl, 1.0);
+	gl_FragColor = vec4(diffuse + spec, 1.0);
 }
